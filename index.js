09d50ba00efac3a10e3e56af902bcac9712e8eca
tooshar <mr.tusharprajapati@gmail.com>
2018-12-09 10:16:35 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+    socket.on('text-message', function (msg) {
+        io.emit('text-message', msg);
+        io.emit('user_id', socket.id)
+
+    });
+
+    socket.on('enable-or-disable-text', function (msg, ) {
+        io.emit('enable-or-disable-text', msg, socket.id)
+    });
+});
+
+
+
+http.listen(3000, function () {
+    console.log('listening on port 3000');
+});
